f247ce61300e8d4f22d52488a7c33bd2de005668
pedromela <pedro.miguel.69@hotmail.com>
2021-11-05 02:11:38 +0000

    Revert "heroku-buildpack-nodejs"
    This reverts commit 91ab441aea9a6d4ea13011bb0e061be53c5ce9c1.

--- etc/ci-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-[ "$CI" != "true" ] && echo "Not running on CI!" && exit 1
-[ "$CIRCLE_PROJECT_USERNAME" != "heroku" ] && echo "Run tests manually for forked PRs." && exit 0
-
-bundle install
-bundle exec hatchet ci:setup
