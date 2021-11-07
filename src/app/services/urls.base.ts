869c61cb440cd9e72b7a38716f756388f7d5ec0a
pedromela <pedro.miguel.69@hotmail.com>
2021-11-07 02:23:37 +0000

    heroku urls

--- src/app/services/urls.base.ts
+++ src/app/services/urls.base.ts
@@ -1,8 +1,8 @@
 export class URLS {
-    static botapiURL: string = "https://m4d32tr4d3.ddns.net/BotAPI/api";
-    static loginapiURL: string = "https://m4d32tr4d3.ddns.net/Login5/api";
-    static backtesterapiURL: string = "https://m4d32tr4d3.ddns.net/BacktesterAPI/api";
-    static backtestersignalrapiURL: string = "https://m4d32tr4d3.ddns.net/BacktesterAPI";
+    static botapiURL: string = "https://megaladonbotapi.herokuapp.com/api";
+    static loginapiURL: string = "https://megaladonloginapi.herokuapp.com/api";
+    static backtesterapiURL: string = "https://megaladonbacktesterapi.herokuapp.com/api";
+    static backtestersignalrapiURL: string = "https://megaladonbacktesterapi.herokuapp.com";
     //static botapiURL: string = "https://localhost:44339/api";
     //static loginapiURL: string = "https://localhost:44321/api";
     //static backtesterapiURL: string = "https://localhost:5001/api";
@@ -11,5 +11,5 @@ export class URLS {
     //static botapiURL: string = "http://localhost/BotAPI/api";
     //static loginapiURL: string = "http://localhost/Login5/api";
     //static botengineapiURL: string = "http://localhost:10000/api";
-    static botengineapiURL: string = "https://m4d32tr4d3.ddns.net:12000/api";
+    static botengineapiURL: string = "https://megaladonbotengineapi.herokuapp.com/api";
 }
