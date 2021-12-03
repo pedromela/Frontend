165e978702d5399262e13fbf47719e74094c05c3
pedromela <pedro.miguel.69@hotmail.com>
2021-12-03 16:52:40 +0000

    redict to https in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const  forceSsl = require('force-ssl-heroku');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/megaladon'));
-
+app.use(forceSsl);
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/megaladon/'}),
 );
